3c79ca5f4afe365036f215890edbf6dee1fcf3c9
JulesSAI <101916605+JulesSAI@users.noreply.github.com>
2025-07-26 17:21:13 -0500

    PROYEC-21 Metodo para registrar cliente

--- ChibchaWeb/settings.py
+++ ChibchaWeb/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
+
+    
     'Clientes',
     'Direcciones',
     'Pagos',
--- ChibchaWeb/urls.py
+++ ChibchaWeb/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Clientes/', include('Clientes.urls'))
+
 ]
