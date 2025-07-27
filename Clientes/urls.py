2d0ea1f0d37e8b70b626d14ddcda212f7afb4a69
JulesSAI <101916605+JulesSAI@users.noreply.github.com>
2025-07-26 22:55:58 -0500

    PROYEC-22 Añadida la funcion para modificar clientes segun ID

--- Clientes/urls.py
+++ Clientes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import registrar_cliente, borrar_cliente
+from .views import registrar_cliente, borrar_cliente, editar_cliente
 from django.shortcuts import render
 
 urlpatterns = [
     path('registrar/', registrar_cliente, name='registrar_cliente'),
     path('exito/', lambda request: render(request, 'clientes/exito.html'), name='registro_exitoso'),
     path('Clientes/<int:cliente_id>/borrar/', borrar_cliente, name='borrar_cliente'),
+    path('editar/<int:cliente_id>/', editar_cliente, name='editar_cliente'),
 ]
--- Clientes/views.py
+++ Clientes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Cliente
 from .forms import ClienteForm
+from django.views.decorators.http import require_POST
 
 def registrar_cliente(request):
     if request.method == 'POST':
@@ -15,7 +16,7 @@ def registrar_cliente(request):
     return render(request, 'clientes/registro.html', {'form': form})
 
 
-
+@require_POST #para que cualquiera no pueda borrar con solo ingresar una url
 def borrar_cliente(request, cliente_id):
     if request.method == 'POST':
         cliente = get_object_or_404(Cliente, id=cliente_id)
@@ -23,3 +24,20 @@ def borrar_cliente(request, cliente_id):
         return JsonResponse({'mensaje': 'Cliente eliminado correctamente'}, status=200)
     else:
         return JsonResponse({'error': 'Método no permitido'}, status=405)
+    
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Cliente
+
+
+def editar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)
+
+    if request.method == 'POST':
+        cliente.nombre = request.POST.get('nombre')
+        cliente.apellido = request.POST.get('apellido')
+        cliente.email = request.POST.get('email')
+        cliente.telefono = request.POST.get('telefono')
+        cliente.save()
+        return redirect('/Clientes/exito/')  # O donde quieras redirigir
+
+    return render(request, 'clientes/editar.html', {'cliente': cliente})
