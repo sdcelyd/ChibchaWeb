08097c9ad85f4453249853c7fe7c4b5ce26251bf
JulesSAI <101916605+JulesSAI@users.noreply.github.com>
2025-07-26 22:32:58 -0500

    PROYEC-23 Funcion para eliminar cliente basado en su ID

--- Clientes/urls.py
+++ Clientes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import registrar_cliente
+from .views import registrar_cliente, borrar_cliente
 from django.shortcuts import render
 
 urlpatterns = [
     path('registrar/', registrar_cliente, name='registrar_cliente'),
     path('exito/', lambda request: render(request, 'clientes/exito.html'), name='registro_exitoso'),
+    path('Clientes/<int:cliente_id>/borrar/', borrar_cliente, name='borrar_cliente'),
 ]
--- Clientes/views.py
+++ Clientes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from .models import Cliente
 from .forms import ClienteForm
 
 def registrar_cliente(request):
@@ -12,3 +13,13 @@ def registrar_cliente(request):
         form = ClienteForm()
 
     return render(request, 'clientes/registro.html', {'form': form})
+
+
+
+def borrar_cliente(request, cliente_id):
+    if request.method == 'POST':
+        cliente = get_object_or_404(Cliente, id=cliente_id)
+        cliente.delete()
+        return JsonResponse({'mensaje': 'Cliente eliminado correctamente'}, status=200)
+    else:
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
